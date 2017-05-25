b2360380faaa398d984c75d907c7a6db017dedeb
Javi Moreno <javi.moreno@capside.com>
2017-05-25 10:34:58 +0200

    Versión .war

--- src/main/java/com/capside/pokemondemo/PokemonApplication.java
+++ src/main/java/com/capside/pokemondemo/PokemonApplication.java
@@ -2,9 +2,16 @@ package com.capside.pokemondemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PokemonApplication {
+public class PokemonApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PokemonApplication.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PokemonApplication.class, args);
