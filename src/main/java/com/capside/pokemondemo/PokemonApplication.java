d1823edf1f1d9c7b5564278d089c23858f9d3d8b
Javi Moreno (@ciberado) <javi@programar.cloud>
2018-03-13 21:22:34 +0100

    Added autoretry if the datasource is not ready.

--- src/main/java/com/capside/pokemondemo/PokemonApplication.java
+++ src/main/java/com/capside/pokemondemo/PokemonApplication.java
@@ -1,12 +1,76 @@
 package com.capside.pokemondemo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
 
 @SpringBootApplication
+@EnableRetry
 public class PokemonApplication {
+
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    private class RetryableDataSourceBeanPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName)
+                throws BeansException {
+            if (bean instanceof DataSource) {
+                bean = new RetryableDataSource((DataSource) bean);
+            }
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName)
+                throws BeansException {
+            return bean;
+        }
+    }
+
+    @Bean
+    public BeanPostProcessor dataSouceWrapper() {
+        return new RetryableDataSourceBeanPostProcessor();
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PokemonApplication.class, args);
     }
+
+    // https://github.com/spring-projects/spring-boot/issues/4779
+    // khudos to https://github.com/dsyer
+    class RetryableDataSource extends AbstractDataSource {
+
+        private final DataSource delegate;
+
+        public RetryableDataSource(DataSource delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        @Retryable(maxAttempts = 10, backoff = @Backoff(multiplier = 2.3, maxDelay = 30000))
+        public Connection getConnection() throws SQLException {
+            return delegate.getConnection();
+        }
+
+        @Override
+        @Retryable(maxAttempts = 10, backoff = @Backoff(multiplier = 2.3, maxDelay = 30000))
+        public Connection getConnection(String username, String password)
+                throws SQLException {
+            return delegate.getConnection(username, password);
+        }
+
+    }
+
 }
